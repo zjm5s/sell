547f304b221f1743329db81d08fb7be8f053f09c
zjm <zjm5s@outlook.com>
2019-09-25 12:05:55 +0800

    Initial commit

---
+++ src/test/java/com/imooc/DemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.imooc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    private final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+    @Test
+    public void contextLoads() {
+        logger.trace("trace...");
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.warn("warn...");
+        logger.error("error...");
+    }
+
+}
